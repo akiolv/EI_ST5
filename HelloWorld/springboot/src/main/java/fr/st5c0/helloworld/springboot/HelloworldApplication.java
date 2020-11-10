6d2f531b00659bd6d376be43396e61bb78f59431
akiolv <antoine.bernard@student-cs.fr>
2020-11-10 16:06:58 +0100

    fix

--- HelloWorld/springboot/src/main/java/fr/st5c0/helloworld/springboot/HelloworldApplication.java
+++ HelloWorld/springboot/src/main/java/fr/st5c0/helloworld/springboot/HelloworldApplication.java
@@ -2,12 +2,19 @@ package fr.st5c0.helloworld.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!";
 	}
 
 }
--- HelloWorld/springboot/src/test/java/fr/st5c0/helloworld/springboot/HelloworldApplicationTests.java
+++ HelloWorld/springboot/src/test/java/fr/st5c0/helloworld/springboot/HelloworldApplicationTests.java
@@ -1,20 +1,13 @@
 package fr.st5c0.helloworld.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloworldApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello world!";
+	@Test
+	void contextLoads() {
 	}
 
 }
