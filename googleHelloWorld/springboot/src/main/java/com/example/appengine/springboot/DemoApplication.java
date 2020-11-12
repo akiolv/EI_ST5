fac26af9f97e247db024ff9d485b3f5d84738764
akiolv <antoine.bernard@student-cs.fr>
2020-11-12 04:55:32 +0100

    tente encore

--- googleHelloWorld/springboot/src/main/java/com/example/appengine/springboot/DemoApplication.java
+++ googleHelloWorld/springboot/src/main/java/com/example/appengine/springboot/DemoApplication.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringbootApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/")
