a79ac04c78639e780372e5df83232dbac68c0501
akiolv <antoine.bernard@student-cs.fr>
2020-11-12 12:06:55 +0100

    test

--- serveurweb/springboot/src/main/java/fr/stproject/appengine/springboot/MappingApplication.java
+++ serveurweb/springboot/src/main/java/fr/stproject/appengine/springboot/MappingApplication.java
@@ -3,6 +3,7 @@ package fr.stproject.appengine.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -24,8 +25,9 @@ public class MappingApplication {
 	}
 
 	@GetMapping("/inputdata")
-	public String saveInputData() {
-		return "data incoming";
+	public String saveInputData(@RequestParam(value = "date", defaultValue = "00-00-0000-00-00-00") String date, @RequestParam(value = "taux", defaultValue = "0") String taux, @RequestParam(value = "id_patient", defaultValue = "00-00-0000-00-00-00") String id_patient) {
+		String test = date + "\n" + taux + "\n" + id_patient;
+		return test;
 	}
 
 }
