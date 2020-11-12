3fe336730ab578f1c0b64d015528d885964eaa6f
akiolv <antoine.bernard@student-cs.fr>
2020-11-12 16:15:42 +0100

    test

--- serveurweb/springboot/src/main/java/fr/stproject/appengine/springboot/MappingApplication.java
+++ serveurweb/springboot/src/main/java/fr/stproject/appengine/springboot/MappingApplication.java
@@ -17,10 +17,7 @@ import java.sql.Statement;
 @RestController
 public class MappingApplication {
 
-	private static SQL interfacesql;
-
 	public static void main(String[] args) {
-		interfacesql = new SQL("/home/anouk9876543210/EI_ST5/serveurweb/springboot/db/db-st5-c0.db");
 		SpringApplication.run(MappingApplication.class, args);
 	}
 
@@ -31,21 +28,21 @@ public class MappingApplication {
 
 	@GetMapping("/test")
 	public String schlag() {
-		String test = "resultat: ";
-		try {
-			Connection con = interfacesql.getCon();
-			Statement stmt = con.createStatement();
-			ResultSet res = stmt.executeQuery("SELECT id_mesure AS idMesure FROM Mesures LIMIT 1");
-			test += res.getString("idMesure");
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		}
-		return test;
+//		String test = "resultat: ";
+//		try {
+//			Connection con = interfacesql.getCon();
+//			Statement stmt = con.createStatement();
+//			ResultSet res = stmt.executeQuery("SELECT id_mesure AS idMesure FROM Mesures LIMIT 1");
+//			test += res.getString("idMesure");
+//		} catch (SQLException throwables) {
+//			throwables.printStackTrace();
+//		}
+		return "test";
 	}
 
 	@GetMapping("/inputdata")
 	public String saveInputData(@RequestParam(value = "date", defaultValue = "0") String date, @RequestParam(value = "taux", defaultValue = "0") String taux, @RequestParam(value = "id_patient", defaultValue = "0") String id_patient) {
-		interfacesql.saveInputDataOnDataBase(date, taux, id_patient );
+//		interfacesql.saveInputDataOnDataBase(date, taux, id_patient );
 		return "lol";
 	}
 }
